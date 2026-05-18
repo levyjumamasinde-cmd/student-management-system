2d0636d99de72ab2284d486b0eefbc9f533580ae
levyjumamasinde-cmd <levyjumamasinde@gmail.com>
2026-05-18 21:49:20 +0300

    Support alphanumeric admission numbers

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 // Structure definition
 struct Student {
     char name[50];
-    int admissionNumber;
+    char admissionNumber[20];
     int age;
     float marks[3];
     float average;
@@ -110,7 +110,7 @@ void addStudent() {
     scanf(" %[^\n]", students[count].name);
 
     printf("Enter admission number: ");
-    scanf("%d", &students[count].admissionNumber);
+    scanf("%s", &students[count].admissionNumber);
 
     printf("Enter age: ");
     scanf("%d", &students[count].age);
@@ -140,7 +140,7 @@ void displayStudents() {
     for(int i = 0; i < count; i++) {
         printf("\n----- Student %d -----\n", i + 1);
         printf("Name: %s\n", students[i].name);
-        printf("Admission Number: %d\n", students[i].admissionNumber);
+        printf("Admission Number: %s\n", students[i].admissionNumber);
         printf("Age: %d\n", students[i].age);
         printf("Average: %.2f\n", students[i].average);
         printf("Grade: %c\n", students[i].grade);
@@ -149,18 +149,18 @@ void displayStudents() {
 
 // Search student
 void searchStudent() {
-    int adm;
+    char adm[20];
     int found = 0;
 
     printf("Enter admission number to search: ");
-    scanf("%d", &adm);
+    scanf("%s", &adm);
 
     for(int i = 0; i < count; i++) {
         if(students[i].admissionNumber == adm) {
 
             printf("\nStudent Found!\n");
             printf("Name: %s\n", students[i].name);
-            printf("Admission Number: %d\n", students[i].admissionNumber);
+            printf("Admission Number: %s\n", students[i].admissionNumber);
             printf("Age: %d\n", students[i].age);
             printf("Average: %.2f\n", students[i].average);
             printf("Grade: %c\n", students[i].grade);
@@ -177,11 +177,11 @@ void searchStudent() {
 
 // Update student
 void updateStudent() {
-    int adm;
+    char adm[20];
     int found = 0;
 
     printf("Enter admission number to update: ");
-    scanf("%d", &adm);
+    scanf("%s", &adm);
 
     for(int i = 0; i < count; i++) {
 
@@ -230,7 +230,7 @@ void bestStudent() {
 
     printf("\n===== BEST PERFORMING STUDENT =====\n");
     printf("Name: %s\n", students[best].name);
-    printf("Admission Number: %d\n", students[best].admissionNumber);
+    printf("Admission Number: %s\n", students[best].admissionNumber);
     printf("Average: %.2f\n", students[best].average);
     printf("Grade: %c\n", students[best].grade);
 }
@@ -247,7 +247,7 @@ void saveToFile() {
 
     for(int i = 0; i < count; i++) {
 
-        fprintf(file, "%s %d %d %.2f %c\n",
+        fprintf(file, "%s %s %d %.2f %c\n",
             students[i].name,
             students[i].admissionNumber,
             students[i].age,
@@ -269,7 +269,7 @@ void loadFromFile() {
 
     count = 0;
 
-    while(fscanf(file, "%s %d %d %f %c",
+    while(fscanf(file, "%s %s %d %f %c",
         students[count].name,
         &students[count].admissionNumber,
         &students[count].age,
